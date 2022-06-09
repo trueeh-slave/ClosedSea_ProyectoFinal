1045de819f98b66b0008b6674a082d1451a9e4e5
trueeh-slave <steamtruesnowace@gmail.com>
2022-06-09 13:00:34 -0500

    ArtPOJO.java created

---
+++ src/main/java/co/edu/unbosque/closedsea_proyectofinal/resource/pojos/ArtPOJO.java
@@ -0,0 +1,44 @@
+package co.edu.unbosque.closedsea_proyectofinal.resource.pojos;
+
+public class ArtPOJO {
+
+    private String name;
+
+    private float price;
+
+    private String imagePath;
+
+    private boolean forSale;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public boolean isForSale() {
+        return forSale;
+    }
+
+    public void setForSale(boolean forSale) {
+        this.forSale = forSale;
+    }
+}
